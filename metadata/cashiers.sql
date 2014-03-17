1df3a650f8653a1119a355a4853aeca7d405dae7
root <root@linux-g8u6.site>
2014-03-17 21:42:03 +0200

    Added untrackedfiles

---
+++ metadata/cashiers.sql
@@ -0,0 +1,5 @@
+ DROP TABLE IF EXISTS `cashiers`;
+CREATE TABLE `cashiers` ( ID INTEGER, Surname VARCHAR(50), Name VARCHAR(50), Fathername VARCHAR(50), 
+Position VARCHAR(50), CardNo VARCHAR(20));
+
+DELETE FROM 'cashiers';0000 0010 0726 8061
---
+++ metadata/dump.sql
@@ -0,0 +1,7 @@
+SQLite version 3.7.8 2011-09-19 14:49:19
+Enter ".help" for instructions
+Enter SQL statements terminated with a ";"
+sqlite> ^Cwqeqweqweqweqwewqewqewqewqewqewqqweqweqweqweqweqweqweqw
+   ...> .exit
+   ...> ;
+sqlite> .exit
